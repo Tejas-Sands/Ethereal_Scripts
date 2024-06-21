66755bab6670dc33da6e75149fb4604b97152baa
Tejas-Sands <tsandwar@gmail.com>
2024-06-21 15:27:30 +0530

    i don't know what i am doing any more--

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,9 +1,8 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleStuff } from "./routers/test";
 import { serverRouter } from "./routers/authen";
 import { makeBlog } from "./routers/cblog";
-import { comment } from "./routers/comment";
+
 
 export const appRouter = createTRPCRouter({
   exam: exampleStuff,
