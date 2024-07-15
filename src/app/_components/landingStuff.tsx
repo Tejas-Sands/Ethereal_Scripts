90d27dd6eb95b6c8f046a2e0102d52e032a8f76b
Tejas-Sands <122252636+Tejas-Sands@users.noreply.github.com>
2024-07-15 15:28:58 +0530

    Updated landingStuff.tsx

--- src/app/_components/landingStuff.tsx
+++ src/app/_components/landingStuff.tsx
@@ -78,13 +78,13 @@ scrollTrigger: {
    
     return <>
     <div className=" flex-col overflow-hidden">
-        <img ref={imgx} className=" h-96 align-middle w-auto" src="https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-    <div className=" bg-sky-500 w-3/4 text-blue-950 text-balance text-2xl p-5 " ref={textl}>Unleash your imagination and share your unique voice! Writing an article lets you explore ideas, tell stories, and inspire others. Dive in, embrace your creativity, and let your thoughts flow. Your story matters!</div>
+        <img ref={imgx} className=" h-96 align-middle w-auto p-3 rounded-md" src="https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+    <div className=" bg-sky-500 w-3/4 text-blue-950 text-balance text-2xl p-5 rounded-md" ref={textl}>Unleash your imagination and share your unique voice! Writing an article lets you explore ideas, tell stories, and inspire others. Dive in, embrace your creativity, and let your thoughts flow. Your story matters!</div>
     </div>
     <center><CreateBlogB  /></center>
     <div className=" mt-16 flex flex-row w-full h-24 lg:h-60 md:h-32 sm:h-28 xl:h-64 overflow-hidden">
-        <img ref={imgref} src="https://images.unsplash.com/photo-1452830978618-d6feae7d0ffa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-        <div className=" bg-blue-500 w-3/4 text-blue-950 text-balance p-5 lg:text-2xl  md:text-xl text-sm " ref={textr}>“There is no greater agony than bearing an untold story inside you.”<br/><i>- Maya Angelou</i></div>
+        <img className=" p-3 rounded-md" ref={imgref} src="https://images.unsplash.com/photo-1452830978618-d6feae7d0ffa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+        <div className=" bg-blue-500 w-3/4 text-blue-950 text-balance p-5 lg:text-2xl  md:text-xl text-sm rounded-md" ref={textr}>“There is no greater agony than bearing an untold story inside you.”<br/><i>- Maya Angelou</i></div>
     </div>
     </>
 }
